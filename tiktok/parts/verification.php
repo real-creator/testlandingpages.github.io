457b2e85cfe885c375f3369cc2b34cc6ebe160e0
real-creator <80911883+real-creator@users.noreply.github.com>
2021-05-07 09:45:32 +0000

    Update verification.php

--- tiktok/parts/verification.php
+++ tiktok/parts/verification.php
@@ -2,7 +2,30 @@
 	<div class="human-verification-inner-wrapper">
 	<h3>Human Verification</h3>
 	<p>To prevent robot abuse of our generator, you are required to complete the human verification by clicking the button below.</p>
-	<div id="fountainG">
+			<div class="row verification-user-info">
+		<div class="col-md-6 col-12">
+			<div class="verification-player-panel">
+				<div class="connected-player-item animated bounceIn animation-delay-200">
+					<div class="connected-label">Username:</div>
+					<div class="connected-player-value connected-username-value"></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 col-12">
+			<div class="console-generation-item console-generation-item-r-1 animated bounceIn animation-delay-600">
+				<img src="img/f-icon.png" class="resource-select-icon" />
+				<div class="console-generation-item-value console-generation-item-value-r-1">0</div>
+				<div class="resource-item-label">Followers</div>
+			</div>
+		</div>
+	</div>
+	<i class="fas fa-spinner fa-spin"></i>
+	<div id="h-v-time-left-wrapper" class="h-v-time-left-wrapper">
+		<span>Time Left:</span>
+		<span id="human_verification_timer_time"></span>
+	</div>
+	<div class="verification-button-wrapper animated bounceIn animation-delay-1000">
+	   <div id="fountainG">
 	<div id="fountainG_1" class="fountainG"></div>
 	<div id="fountainG_2" class="fountainG"></div>
 	<div id="fountainG_3" class="fountainG"></div>
@@ -13,7 +36,8 @@
 	<div id="fountainG_8" class="fountainG"></div>
 	</div>
 	<div class="human-verification-captcha-locker-wrapper">
-		<div data-captcha-enable="true"></div>
+	<div data-captcha-enable="true"></div>
 	 </div>
 	</div>
+</div>
 </div>
