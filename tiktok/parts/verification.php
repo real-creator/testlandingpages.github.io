6c13e44bd351f3d30f497756cf21ceb103d2251d
real-creator <80911883+real-creator@users.noreply.github.com>
2021-05-07 09:38:36 +0000

    Update verification.php

--- tiktok/parts/verification.php
+++ tiktok/parts/verification.php
@@ -1,31 +1,19 @@
 <div class="human-verification-wrapper">
+	<div class="human-verification-inner-wrapper">
 	<h3>Human Verification</h3>
-	<p>You are almost done! Click on the Verify Now button below to complete human verification.</p>
-	<div class="row verification-user-info">
-		<div class="col-md-6 col-12">
-			<div class="verification-player-panel">
-				<div class="connected-player-item animated bounceIn animation-delay-200">
-					<div class="connected-label">Username:</div>
-					<div class="connected-player-value connected-username-value"></div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-6 col-12">
-			<div class="console-generation-item console-generation-item-r-1 animated bounceIn animation-delay-600">
-				<img src="img/f-icon.png" class="resource-select-icon" />
-				<div class="console-generation-item-value console-generation-item-value-r-1">0</div>
-				<div class="resource-item-label">Followers</div>
-			</div>
-		</div>
-	</div>
-	<i class="fas fa-spinner fa-spin"></i>
-	<div id="h-v-time-left-wrapper" class="h-v-time-left-wrapper">
-		<span>Time Left:</span>
-		<span id="human_verification_timer_time"></span>
+	<p>To prevent robot abuse of our generator, you are required to complete the human verification by clicking the button below.</p>
+	<div id="fountainG">
+	<div id="fountainG_1" class="fountainG"></div>
+	<div id="fountainG_2" class="fountainG"></div>
+	<div id="fountainG_3" class="fountainG"></div>
+	<div id="fountainG_4" class="fountainG"></div>
+	<div id="fountainG_5" class="fountainG"></div>
+	<div id="fountainG_6" class="fountainG"></div>
+	<div id="fountainG_7" class="fountainG"></div>
+	<div id="fountainG_8" class="fountainG"></div>
 	</div>
 	<div class="human-verification-captcha-locker-wrapper">
-	<div data-captcha-enable="true"></div>
-
-		
+		<div data-captcha-enable="true"></div>
+	 </div>
 	</div>
 </div>
